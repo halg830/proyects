c9bdfde9549ec8e89a59777a9d2e959c12d25079
halg830 <halg830@gmail.com>
2023-06-22 01:11:35 -0500

    rama exercise

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+import {ref} from 'vue'
+
   const name = "Hector"
   const styleColor = "color: blue"
   const arrayColors = ["blue", "red", "orange"]
@@ -26,6 +28,13 @@
 
 const clic = () =>{
   console.log("clic")
+}
+
+//ref devuelve un objecto
+let counter = ref(0)
+
+const aumentar = () =>{
+  counter.value++
 }
 </script>
 
@@ -59,6 +68,8 @@ const clic = () =>{
   </ul>
   <button @click="clic">Activame</button>
   <button @click.right="clic">Activame right</button>
+  <h2>{{ counter }}</h2>
+  <button @click="aumentar">Aumentar</button>
 </template>
 
 <style>
@@ -66,3 +77,58 @@ const clic = () =>{
     color: red
   }
 </style>
+
+<!-- 
+CUANDO USAMOS ALGO CON REACTIVIDAD ES MEJOR USAR COMPUTED
+
+  <script setup>
+import { ref, computed } from "vue";
+
+const name = "Vue 3";
+
+const counter = ref(0);
+
+const increment = () => {
+    counter.value++;
+};
+
+const decrement = () => {
+    counter.value--;
+};
+
+const reset = () => {
+    counter.value = 0;
+};
+
+const classCounter = computed(() => {
+    if (counter.value === 0) {
+        return "zero";
+    }
+    return counter.value > 0 ? "positive" : "negative";
+});
+
+</script>
+
+<template>
+    <h1>Hola {{ name }}!</h1>
+    <h2 :class="classCounter">
+        {{ counter }}
+    </h2>
+    <button @click="increment">Incremet</button>
+    <button @click="decrement">Decrement</button>
+    <button @click="reset">Reset</button>
+</template>
+
+<style>
+.negative {
+    color: red;
+}
+
+.positive {
+    color: green;
+}
+
+.zero {
+    color: black;
+}
+</style> -->
